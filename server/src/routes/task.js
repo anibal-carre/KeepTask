56566cdd660d77726b092f2565ca808ce7ec1dcb
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-02-07 19:53:19 -0300

    task middleware

--- server/src/routes/task.js
+++ server/src/routes/task.js
@@ -6,13 +6,14 @@ import {
   editTask,
   deleteTask,
 } from "../controllers/task.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/create", createTask);
-router.put("/edit/:_id", editTask);
-router.get("/:user_id", getTasks);
-router.get("/task/:_id", findTask);
-router.delete("/delete/:_id", deleteTask);
+router.post("/create", verifyToken, createTask);
+router.put("/edit/:_id", verifyToken, editTask);
+router.get("/:user_id", verifyToken, getTasks);
+router.get("/task/:_id", verifyToken, findTask);
+router.delete("/delete/:_id", verifyToken, deleteTask);
 
 export default router;
