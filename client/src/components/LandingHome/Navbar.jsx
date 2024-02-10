4214e072c89cc921521f2ae4c4ea8eee5e91ce56
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-02-10 20:27:31 -0300

    header added

--- client/src/components/LandingHome/Navbar.jsx
+++ client/src/components/LandingHome/Navbar.jsx
@@ -1,9 +1,40 @@
-import logow from "../../assets/logowhite.png";
+import MenuIcon from "../Icons/MenuIcon";
+import NoteIcon from "../Icons/NoteIcon";
 
 const Navbar = () => {
   return (
-    <nav className="w-full h-[80px] flex items-center px-3 border border-zinc-700">
-      <img src={logow} alt="logo" className="w-[100px] h-[120px]" />
+    <nav className="w-full h-[80px] flex items-center justify-between px-5 md:p-3 border border-zinc-200 shadow-md">
+      <div className="flex items-center">
+        <NoteIcon />{" "}
+        <span className="text-blue-400 text-[30px] font-[700] cursor-pointer">
+          KeepTask
+        </span>
+      </div>
+
+      <div className="hidden md:flex">
+        <nav>
+          <ul className="flex items-center gap-3">
+            <li className="mr-3">
+              <span className="text-blue-400 text-[18px] font-[600] cursor-pointer">
+                Docs
+              </span>
+            </li>
+
+            <li className="mr-3">
+              <span className="text-blue-400 text-[18px] font-[600] cursor-pointer">
+                Blog
+              </span>
+            </li>
+
+            <div className="w-[2px] rounded-sm h-[45px] bg-blue-300 mr-2"></div>
+
+            <button className="w-[100px] h-[40px] bg-blue-400 hover:bg-blue-300 rounded-[8px] text-[18px] font-[600] text-white">
+              Login
+            </button>
+          </ul>
+        </nav>
+      </div>
+      <MenuIcon />
     </nav>
   );
 };
--- client/src/scenes/LandingHome.jsx
+++ client/src/scenes/LandingHome.jsx
@@ -3,8 +3,9 @@ import Navbar from "../components/LandingHome/Navbar";
 const LandingHome = () => {
   return (
     <div className="w-full h-scree">
-      <Navbar />
-      <h1 className="text-green-500 text-[40px]">Landing Home</h1>
+      <header className="w-full">
+        <Navbar />
+      </header>
     </div>
   );
 };
