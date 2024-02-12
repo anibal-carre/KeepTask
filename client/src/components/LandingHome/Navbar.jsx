6d4b9a85d225611838107c52023de15e83179ad8
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-02-12 11:57:38 -0300

    framer motion added

--- client/src/components/LandingHome/Navbar.jsx
+++ client/src/components/LandingHome/Navbar.jsx
@@ -1,17 +1,28 @@
+import AccountIcon from "../Icons/AccountIcon";
 import MenuIcon from "../Icons/MenuIcon";
 import NoteIcon from "../Icons/NoteIcon";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <nav className="w-full h-[80px] flex items-center justify-between px-5 md:p-3 border border-zinc-200 shadow-md">
       <div className="flex items-center">
-        <NoteIcon />{" "}
+        <motion.div
+          animate={{
+            scale: [1, 1, 2, 1, 1],
+            rotate: [0, 0, 60, 50, 0],
+          }}
+          transition={{ ease: "easeInOut", duration: 0.7 }}
+        >
+          <NoteIcon />{" "}
+        </motion.div>
         <span className="text-blue-400 text-[30px] font-[700] cursor-pointer">
           KeepTask
         </span>
       </div>
 
       <div className="hidden md:flex">
+        <button className="bg-blue-400">hello</button>
         <nav>
           <ul className="flex items-center gap-3">
             <li className="mr-3">
@@ -28,8 +39,8 @@ const Navbar = () => {
 
             <div className="w-[2px] rounded-sm h-[45px] bg-blue-300 mr-2"></div>
 
-            <button className="w-[100px] h-[40px] bg-blue-400 hover:bg-blue-300 rounded-[8px] text-[18px] font-[600] text-white">
-              Login
+            <button className="w-[100px] h-[40px] flex justify-center items-center bg-blue-400 hover:bg-blue-300 rounded-[8px] text-[18px] font-[600] text-white">
+              <AccountIcon />
             </button>
           </ul>
         </nav>
--- client/src/scenes/LandingHome.jsx
+++ client/src/scenes/LandingHome.jsx
@@ -1,11 +1,54 @@
 import Navbar from "../components/LandingHome/Navbar";
+import { animate, motion } from "framer-motion";
 
 const LandingHome = () => {
+  const titleAnimation = {
+    initial: {
+      opacity: 0.2,
+      y: -60,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+    },
+  };
   return (
     <div className="w-full h-scree">
       <header className="w-full">
         <Navbar />
       </header>
+
+      <div className="w-full mt-20 flex flex-col items-center px-5">
+        <motion.h1
+          variants={titleAnimation}
+          initial="initial"
+          animate="animate"
+          transition={{ ease: "easeInOut", duration: 0.6 }}
+          className="text-blue-400 font-[700] text-[30px] md:text-[55px] mb-5"
+        >
+          Organize your tasks and notes with KeepTask
+        </motion.h1>
+
+        <motion.span
+          variants={titleAnimation}
+          initial="initial"
+          animate="animate"
+          transition={{ ease: "easeInOut", duration: 0.9 }}
+          className="text-blue-300 text-[18px] md:text-[25px] font-[600]"
+        >
+          A task manager with a modern and creative interface
+        </motion.span>
+
+        <motion.button
+          variants={titleAnimation}
+          initial="initial"
+          animate="animate"
+          transition={{ ease: "easeInOut", duration: 0.9 }}
+          className="w-[250px] h-[50px] bg-blue-400 text-[18px] font-[600] text-white rounded-[8px] mt-20 hover:bg-blue-300"
+        >
+          Get started
+        </motion.button>
+      </div>
     </div>
   );
 };
